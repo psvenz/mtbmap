3efeee3fba8d9e3c6a0eec8cb74c08d3fea7d8ca
psvenz <svensson3000@gmail.com>
2014-12-14 21:59:19 +0100

    renamed bat file. Zoom=15

--- mtbmap.se production scandinavia .bat
+++
@@ -1,2 +0,0 @@
-java -jar osmtilemachine.jar -source downloadeurope -renderarea scandinavia -generatelowzoom -webroot z:\ -maxzoom 16 -verbose
-pause
---
+++ mtbmap.se production.bat
@@ -0,0 +1,3 @@
+java -jar osmtilemachine.jar -firstaction 43744 -source downloadplanet -renderarea scandinavia -generatelowzoom -webroot W:\tiles.mtbmap.se\ -maxzoom 15 -minfilesize 874 -verbose
+ 
+pause
