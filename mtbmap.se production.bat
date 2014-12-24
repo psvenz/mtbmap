c840667397a16ac05b821081cce3c1a5ac94498f
psvenz <svensson3000@gmail.com>
2014-12-24 22:36:20 +0100

    fixed bat file

--- mtbmap.se production.bat
+++ mtbmap.se production.bat
@@ -1,3 +1,3 @@
-java -jar osmtilemachine.jar -firstaction 43744 -source downloadplanet -renderarea scandinavia -generatelowzoom -webroot W:\tiles.mtbmap.se\ -maxzoom 15 -minfilesize 874 -verbose
+java -jar osmtilemachine.jar -firstaction 1 -source downloadplanet -renderarea scandinavia -generatelowzoom -webroot W:\tiles.mtbmap.se\ -maxzoom 15 -minfilesize 874 -verbose
  
 pause
