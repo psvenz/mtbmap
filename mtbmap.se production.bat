71139507bf5cda23dd7116cec3aa79bfc5790672
psvenz <svensson3000@gmail.com>
2015-02-21 08:31:35 +0100

    Updated bat file

--- mtbmap.se production.bat
+++
@@ -1,3 +0,0 @@
-java -jar osmtilemachine.jar -firstaction 1 -source downloadplanet -renderarea scandinavia -generatelowzoom -webroot W:\tiles.mtbmap.se\ -maxzoom 15 -minfilesize 874 -verbose
- 
-pause
---
+++ mtbmap.se production_scandinavia.bat
@@ -0,0 +1,4 @@
+java -jar osmtilemachine.jar -rules mtbmap.se.rules -source downloadplanet -renderarea scandinavia -webroot W:\tiles.mtbmap.se\ -maxzoom 15 -minfilesize 874 -verbose
+
+
+pause
