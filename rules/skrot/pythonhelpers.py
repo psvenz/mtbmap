a91ffa50dd40e53ac9496af2053816076eafd214
psvenz <svensson3000@gmail.com>
2016-01-23 15:10:56 +0100

    cleanup.

--- rules/skrot/pythonhelpers.py
+++
@@ -1,5 +0,0 @@
-def routename(e):
-    str_list = []
-    i = 0
-    for set in e.tagSets:
-         return set.ToString()
--- rules/skrot/test.py
+++
@@ -1,8 +0,0 @@
-def cycleLabel(e):
-    str_list = []
-    for set in e.tagSets:
-        if set.hasTag('ref'):
-            str_list.append(set['ref'])
-
-    str_list.sort()
-    return '+'.join(str_list)
