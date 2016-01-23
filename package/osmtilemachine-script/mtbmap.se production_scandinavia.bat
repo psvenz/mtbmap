3b2804a35d2e068fbacec365725fec2071cd02fd
psvenz <svensson3000@gmail.com>
2016-01-23 22:44:22 +0100

    included script for test area

--- package/osmtilemachine-script/mtbmap.se production_scandinavia.bat
+++
@@ -1,4 +0,0 @@
-java -jar osmtilemachine.jar -rules mtbmap.se.rules -source downloadplanet -renderarea scandinavia -webroot W:\tiles.mtbmap.se\ -maxzoom 15 -minfilesize 874 -verbose
-
-
-pause
---
+++ package/osmtilemachine-script/mtbmap.se scandinavia.bat
@@ -0,0 +1,4 @@
+java -jar osmtilemachine.jar -rules mtbmap-nextgeneration.mrules -source downloadplanet -renderarea scandinavia -webroot Z:\production\beta.mtbmap.se\ -maxzoom 15 -minfilesize 874 -verbose
+
+
+pause
