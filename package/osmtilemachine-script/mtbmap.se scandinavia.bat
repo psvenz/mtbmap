f461fcc33ac006d16dfb7e33d7e49e0f9210feb7
psvenz <svensson3000@gmail.com>
2016-01-27 20:37:46 +0100

    Fixed rowbrake

--- package/osmtilemachine-script/mtbmap.se scandinavia.bat
+++ package/osmtilemachine-script/mtbmap.se scandinavia.bat
@@ -1,4 +1,5 @@
-java -jar osmtilemachine.jar -rules mtbmap-nextgeneration.mrules -source downloadplanet -renderarea scandinavia -webroot Z:\production\beta.mtbmap.se\ -maxzoom 15 -minfilesize 874 -verbose
+java -jar osmtilemachine.jar -rules mtbmap-nextgeneration.mrules -source downloadplanet -renderarea scandinavia -webroot Z:\production\beta.mtbmap.se\ -maxzoom 17 -minfilesize 874 -verbose
+
 
 
 pause
--- package/osmtilemachine-script/mtbmap.se stockholm test.bat
+++ package/osmtilemachine-script/mtbmap.se stockholm test.bat
@@ -1,4 +1,5 @@
 java -jar osmtilemachine.jar -rules mtbmap-nextgeneration.mrules -source downloadsweden -renderarea box=17.6;59.1;18.6;59.6 -webroot Z:\production\beta.mtbmap.se\ -maxzoom 15 -minfilesize 874 -verbose
 
 
+
 pause
